e1305670368cd19a2d740f2b0b352b7e976b941a
silvertheo-coder <theokala@orange.fr>
2020-09-20 10:11:11 +0200

    error handlers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,28 @@
+const User = require('../models/User')
 
+// handle errors
+const handleErrors = (err) => {
+   console.log(err.message, err.code);
+   // message to send to the user
+   let errors = { email: '', password: '' }
+
+   // duplicate error code
+   if(err.code === 11000){
+      errors.email = 'That email already exists'
+      return errors;
+   }
+
+   // validation errors
+   if (err.message.includes('user validation failed')) {
+      Object.values(err.errors).forEach(({ properties }) => {
+        errors[properties.path] = properties.message;
+         
+      });
+      
+   }
+   return errors;
+   
+}
 
 module.exports.signup_get = (req, res) => {
    res.render('signup')
@@ -6,9 +30,22 @@ module.exports.signup_get = (req, res) => {
 module.exports.login_get = (req, res) => {
    res.render('login')
 }
-module.exports.signup_post = (req, res) => {
-   res.send('new signup')
+module.exports.signup_post = async (req, res) => {
+   const { email, password } = req.body
+   
+   try{
+      // creates an instance of a user and save it to the DB
+      const user = await User.create({ email, password })
+      res.status(201).json(user)
+   }
+   catch(err){
+      const errors = handleErrors(err)
+      res.status(400).json({ errors })
+      
+   }
 }
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
+   const { email, password } = req.body
+
    res.send('user login')
 }
